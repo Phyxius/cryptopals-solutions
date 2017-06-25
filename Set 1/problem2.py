abffecb9648b130be6f31750df2250ad731cdf53
Shea Polansky <shea@polansky.co>
2017-06-24 21:59:07 -0400

    Add problem 2 solution

---
+++ Set 1/problem2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Solution for Problem 1 of Set 1 of the Cryptopals Challenges
+(C) Shea Polansky, licensed under GPLv3
+2017-06-24
+
+Usage: ./problem1.py <hexbuffer> <hexbuffer>
+Output: bitwise XOR of each byte in the buffer, hex encoded
+"""
+
+import sys, base64, itertools
+
+lhs = base64.b16decode(sys.argv[1], True)
+rhs = base64.b16decode(sys.argv[2], True)
+
+output = [a ^ b for a,b in zip(lhs, rhs)]
+print(base64.b16encode(bytes(output)).decode("utf8"))
